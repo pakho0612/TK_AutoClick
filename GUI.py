b39bdf3929b92613f0c7353b95f79834ce5178a2
Vincent Cheung <vvincentcheungg@gmail.com>
2022-04-15 17:28:35 -0400

    task time button

--- GUI.py
+++ GUI.py
@@ -7,22 +7,39 @@ class GUI(QtWidgets.QWidget):
     def __init__(self):
        super().__init__(); # invoke init of super class(QtWidgets.QWidget), GUI() is child class of QtWidgets.QWidget
 
-       LoadTroopsButton = QtWidgets.QPushButton(self);
-       LoadTroopsButton.setText("Load Troops");
-       LoadTroopsButton.move(50,20);
-       LoadTroopsButton.clicked.connect(self.LoadTroops);
+       self.Troop1 = ''; ## store location to the PNG
+       self.Troop2 = '';
+       self.Troop3 = '';
+       self.Troop4 = '';
+       self.Troop5 = '';
+       self.LoadTroopsButton = QtWidgets.QPushButton(self);
+       self.LoadTroopsButton.setText("Load Troops");
+       self.LoadTroopsButton.move(50,20);
+       self.LoadTroopsButton.clicked.connect(self.LoadTroops);
+
+       self.TaskTimeMenu = QtWidgets.QDateTimeEdit(self);
+       self.TaskTimeMenu.setCalendarPopup(True); ## calendar pop up for date setting
+       self.TaskTimeMenu.setDisplayFormat("yyyy-MM-dd hh:mm:ss ap");
+       self.TaskTime = QtCore.QDateTime.currentDateTime();
+       self.TaskTimeMenu.setDateTime(self.TaskTime);
+       self.TaskTime = self.TaskTime.toPyDateTime();
+       self.TaskTimeMenu.dateTimeChanged.connect(self.TaskTimeChanged);
             
-       self.setGeometry(10,10,300,200);
-       self.setWindowTitle('PyQt');
+       self.setGeometry(400,400,300,200); ##GUI window location and size
+       self.setWindowTitle('TKAutoClick');
        self.show();
 
     def LoadTroops(self):
         print('Loading Troops');
 
+    def TaskTimeChanged(self):
+        self.TaskTime = self.TaskTimeMenu.dateTime().toPyDateTime();
+        print('Task Time has changed to :', self.TaskTime);
+
 def GUI_Init():
     app = QtWidgets.QApplication(sys.argv);# contain the GUI application object
     gui = GUI();
-    sys.exit(app.exec_())
+    sys.exit(app.exec_());
     return gui;
 	
 if __name__ == '__main__':
